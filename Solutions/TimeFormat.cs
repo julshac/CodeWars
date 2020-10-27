d714a53a6ed82ad33ece4ac6d13c000e4898a107
julshac <julia7476@gmail.com>
2020-10-27 15:32:41 +0300

    Refactor GetReadableTime from Solutions

--- Solutions/TimeFormat.cs
+++ Solutions/TimeFormat.cs
@@ -6,17 +6,13 @@ namespace Solutions
     {
         public static string GetReadableTime(int seconds)
         {
-            int hours, minutes, sec;
-            string res = "";
-            hours = seconds / 3600;
-            if (hours < 10) res += "0" + hours + ":";
-            else res += hours + ":";
-            minutes = (seconds - (3600 * hours)) / 60;
-            if (minutes < 10) res += "0" + minutes + ":";
-            else res += minutes + ":";
-            sec = seconds - (3600 * hours) - (minutes * 60);
-            if (sec < 10) res += "0" + sec;
-            else res += sec; 
+            var res = "";
+            var hours = seconds / 3600;
+            res += hours < 10 ? "0" + hours + ":" : hours + ":";
+            var minutes = (seconds - (3600 * hours)) / 60;
+            res += minutes < 10 ? "0" + minutes + ":" : minutes + ":";
+            var sec = seconds - (3600 * hours) - (minutes * 60);
+            res += sec < 10 ? "0" + sec : sec.ToString();
             return res;
         }
     }
